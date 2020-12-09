4d0cd9e445cc6357ef6c19102fce51048f8b6264
kitamumin <bjrk1290230@gn.iwasaki.ac.jp>
2020-12-09 14:19:05 +0900

    release ver 1.0

--- calc.js
+++ calc.js
@@ -1,19 +1,38 @@
+equal_state = false;
+state='start'
 const num_bth = document.querySelectorAll('num');
 let output_sub = document.getElementById('expr');
 const output_total = document.getElementById('result');
 let total = 0;
 
+convert = (text, flg) => {
+  if (flg) {
+    text = text.replace("/", "÷");
+    text = text.replace("*", "×");
+  } else {
+    text = text.replace("×", "*");
+    text = text.replace("÷", "/");
+  }
+  return text;
+}
 
 const one = document.querySelectorAll('.kazu');
     one.forEach(index => {     
       index.addEventListener('click', () => {
-        console.log(index.dataset.indexId)
+
+        if(equal_state) {
+          total = 0
+          output_sub.textContent = total;
+          equal_state = false;
+        }
+
         if(total === 0) {
           total = index.dataset.indexId;  
         }else{
           total += index.dataset.indexId;
-        }      
-        output_sub.textContent = total;
+        }     
+        //console.log(total)
+        output_sub.textContent = convert(total, true);
         state = 'kazu'//数字を入力している状態にする。
       })
     })
@@ -27,13 +46,13 @@ const one = document.querySelectorAll('.kazu');
     if(state === 'kigou'){
       return;
     }
-    console.log(zero.dataset.indexId)
+    //console.log(zero.dataset.indexId)
     if(total === 0) {
       total = zero.dataset.indexId;  
     }else{
       total += zero.dataset.indexId;
     }      
-    output_sub.textContent = total;
+    output_sub.textContent = convert(total, true);
     state = 'kazu'//数字を入力している状態にする。
   })   
 
@@ -47,13 +66,18 @@ const one = document.querySelectorAll('.kazu');
       if(state === 'kigou'){
         return;
       }
-      console.log(index.dataset.indexId)
+
+      if(equal_state) {
+        total = eval(convert(output_sub.textContent, false));
+        equal_state = false;
+      }
+      //console.log(index.dataset.indexId)
       if(total === 0) {
         total = index.dataset.indexId;  
       }else{
         total += index.dataset.indexId;
-      }      
-      output_sub.textContent = total;
+      } 
+      output_sub.textContent = convert(total, true);
       state = 'kigou'//演算記号を入力している状態する。
     })
   })
@@ -61,8 +85,10 @@ const one = document.querySelectorAll('.kazu');
   //イコールを押した時
   const equal_btn = document.getElementById('equal_btn');
   equal_btn.addEventListener('click',() =>{
-    console.log(eval(total));
-    output_total.textContent = eval(total);
+    //console.log(eval(total));
+    output_total.textContent = eval(convert(total, false));
+
+    equal_state = true;
   });
 
   const clear = document.getElementById('ac')
@@ -76,4 +102,5 @@ const one = document.querySelectorAll('.kazu');
    expr.textContent = 0;
    result.textContent = 0;
    state = 'start';
+   equal_state = false;
   }
